85fbf8b4d81f6a3ae1182042c71464b921d35452
Shinnosuke1465 <s.shinno0605@icloud.com>
2023-07-07 17:00:04 +0900

    .htaccessの設定の反映

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -1,18 +1,21 @@
 <?php
-require_once '../config/config.php';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="<?php echo BASE_CSS_PATH ?>style.css">
-</head>
-<body>
-  hello
-  <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg" alt="">
-  <script src="<?php echo BASE_JS_PATH ?>sample.js"></script>
-</body>
-</html>
+// dirname(__DIR__)...今いるディレクトリの親のディレクトリを返す（appディレクトリ）
+require_once( dirname(__DIR__) .'/config/config.php');
+
+
+require_once( dirname(__DIR__) .'/htdocs/php/partials/header.php');
+$request_uri = $_SERVER['REQUEST_URI'];
+echo $_SERVER['REQUEST_URI'];
+// 対象の文字列(第３引数)と第一引数の文字列とマッチした文字を第二引数で置換する
+// 例 localhost/loginとアクセスした場合。$_SERVER['REQUEST_URI']=/login $uri=/のためマッチする/が空文字に変換され$rpath＝loginとなる
+if($_SERVER['REQUEST_URI'] === "/login") {
+    require_once(dirname(__DIR__).'/htdocs/php/controllers/login.php');
+} elseif (mb_strpos($_SERVER['REQUEST_URI'] , "/register") !== false){
+    require_once(dirname(__DIR__) . '/htdocs/php/controllers/register.php');
+} elseif (mb_strpos($_SERVER['REQUEST_URI'] , "/") !== false) {
+    require_once(dirname(__DIR__) . '/htdocs/php/controllers/home.php');
+}
+
+require_once( dirname(__DIR__) .'/htdocs/php/partials/footer.php');
+?>
---
+++ app/htdocs/php/controllers/home.php
@@ -0,0 +1,8 @@
+<!-- viewsフォルダのhome.phpの内容を表示する -->
+<!-- index.phpで読み込むためのファイル(mvc) -->
+
+<?php
+
+#localhost(htdocs)/php/views/home.phpと同じ
+require_once(dirname(__DIR__)."/views/home.php");
+?>
